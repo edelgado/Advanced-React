bfa5b05d72cee7958e44de66289d306075b1f924
Enrique Delgado <delgado.enrique@gmail.com>
2018-11-08 13:11:02 -0700

    My code for Step 4

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,12 +1,7 @@
-import Link from 'next/link'
-
-const Home = props => (
+const Home = () => (
   <div>
     <p>Home!</p>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
   </div>
-)
+);
 
-export default Home
+export default Home;
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,12 +1,8 @@
-import Link from 'next/link'
-
-const Sell = props => (
+const Sell = () => (
   <div>
     <p>Sell!</p>
-    <Link href="/">
-      <a>Home!</a>
-    </Link>
-  </div>
-)
 
-export default Sell
+  </div>
+);
+
+export default Sell;
